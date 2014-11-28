7b6ee946653072b7fb6b56ad2f1b39a59e3d0a50
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2014-11-28 09:30:43 +0100

    xf86-video-sis: Fix build with xserver 1.14 (pass #2)

--- x11-drivers/xf86-video-sis/files/patch-src_sis__driver.c
+++ x11-drivers/xf86-video-sis/files/patch-src_sis__driver.c
@@ -16,3 +16,25 @@
      xf86SetBackingStore(pScreen);
      xf86SetSilkenMouse(pScreen);
  
+@@ -9351,11 +9349,12 @@ SISMergedPointerMoved(SCRN_ARG_TYPE arg,
+ 	}
+      }
+      if(doit) {
++#if GET_ABI_MAJOR(ABI_XINPUT_VERSION) < 20 /* screw it */
+ 	sigstate = xf86BlockSIGIO();
+ #if GET_ABI_MAJOR(ABI_XINPUT_VERSION) >= 15
+         {
+             double dx = x, dy = y;
+-            miPointerSetPosition(inputInfo.pointer, Absolute, &dx, &dy);
++            miPointerSetPosition(inputInfo.pointer, Absolute, &dx, &dy, NULL, NULL);
+             x = (int)dx;
+             y = (int)dy;
+         }
+@@ -9368,6 +9367,7 @@ SISMergedPointerMoved(SCRN_ARG_TYPE arg,
+ 	miPointerAbsoluteCursor(x, y, currentTime.milliseconds);
+ #endif
+ 	xf86UnblockSIGIO(sigstate);
++#endif
+ 	return;
+      }
+   }
